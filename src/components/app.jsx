c03c3eeb2bbc1519e98704516017145e18eb1491
Thiago Almeida <thiago.almeida@coopersystem.com.br>
2020-01-27 17:28:21 -0300

    feat:component_menu_a45

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import About from './about/about';
+import Todo from './todo/todo';
+import Menu from '../template/menu';
 
-const App = ({FontAwesomeIcon}) =>
+
+const App = () =>
 (
-  <div className='container'>
-     <h1>Teste</h1>
+  <div className="container">
+    
+
+    <Menu />  
+    <Todo />
+    <About />
+
   </div>
 )
 
---
+++ src/template/menu.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import fontawesome from '@fortawesome/fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { fas } from '@fortawesome/free-solid-svg-icons'
+// import { far } from '@fortawesome/free-regular-svg-icons'
+// import { fab } from '@fortawesome/free-brands-svg-icons'
+
+// // Add both three icon sets
+// library.add(fas, far, fab)
+
+// console.log(Object.keys(library.definitions.fas))
+// console.log(Object.keys(library.definitions.far))
+// console.log(Object.keys(library.definitions.fab))
+
+
+export default  props =>  (
+ 
+    <div className="container-fluid">
+      
+    <nav className='navbar navbar-expand-lg navbar-light bg-light d-flex align-content-center justify-content-start'>
+    <FontAwesomeIcon icon="fa-camera" size="1x" />
+        <a href="#" className="navbar-brand">Navbar</a>    
+    </nav>
+
+    </div>
+ )   
